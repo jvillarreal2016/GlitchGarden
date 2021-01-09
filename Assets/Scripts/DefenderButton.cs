f0caf63ebfbf6effa291f1998a17a3db6faae336
Javier <jvillarreal2016@gmail.com>
2021-01-08 22:21:26 -0600

    First Commit

---
+++ Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DefenderButton : MonoBehaviour
+{ 
+
+    [SerializeField] Defender defenderPrefab;
+
+    private void Start()
+    {
+        LabelButtonWithCost();
+    }
+
+    private void LabelButtonWithCost()
+    {
+        Text costText = GetComponentInChildren<Text>();
+        if(!costText)
+        {
+            Debug.Log(name + " has no cost text, add some!");
+        }
+        else
+        {
+            costText.text = defenderPrefab.GetStarCost().ToString();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        DisplayStar[] buttons = FindObjectsOfType<DisplayStar>();
+        foreach(DisplayStar button in buttons)
+        {
+            button.GetComponent<SpriteRenderer>().color = new Color32(41, 41, 41, 255);
+        }
+
+        GetComponent<SpriteRenderer>().color = Color.white;
+        FindObjectOfType<DefenderSpawner>().SetSelectedDefender(defenderPrefab);
+    }
+
+}
